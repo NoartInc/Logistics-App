860f2e3b015ce9e4749c3e206a42d22f0e19ef06
Afif <incnoart@gmail.com>
2022-08-19 14:24:53 +0700

    fix: typo TrackPengiriman Migration

--- back-end/migrations/20220720130803-create-track-pengiriman.js
+++ back-end/migrations/20220720130803-create-track-pengiriman.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('trackPengirimans', {
+    await queryInterface.createTable('TrackPengirimans', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -31,6 +31,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('trackPengirimans');
+    await queryInterface.dropTable('TrackPengirimans');
   }
 };
