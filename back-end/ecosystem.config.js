6f878a867f62431fd8cb55daad0dbc7faa0f7f10
Afif <incnoart@gmail.com>
2022-08-20 20:03:14 +0700

    fixin datatype

---
+++ back-end/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps : [{
+      name: "be-logistic",
+      script: "npm start",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      }
+    }]
+  }
