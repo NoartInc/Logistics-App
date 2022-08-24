8887bcd83d254a164377e1e703e63d01f61e04b3
Noartinc <incnoart@gmail.com>
2022-08-24 07:47:09 +0000

    upload dari server to github

--- front-end/http-common.js
+++ front-end/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from "./src/store";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "http://transmetalroof.com:5000/",
   headers: {
     "Content-type": "application/json"
   }
