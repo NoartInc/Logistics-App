21d248843872e7d779aa99d605992d890c82963f
Afif <incnoart@gmail.com>
2023-01-05 15:21:24 +0700

    update subtract 90days kebelakang

--- back-end/controllers/Pengiriman.js
+++ back-end/controllers/Pengiriman.js
@@ -92,7 +92,7 @@ exports.findAllPengiriman = async (req, res) => {
 
     const offset = (page - 1) * limit;
     const startDate = moment()
-      .subtract(30, "days")
+      .subtract(90, "days")
       .format("YYYY-MM-DD HH:mm:ss");
     const endDate = moment().format("YYYY-MM-DD HH:mm:ss");
     let formatedDateSearch = "";
